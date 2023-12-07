7b89685409abcbae31ec4dfe9a435ec1c8ce35b5
jjackchoi <jackchoi0325@gmail.com>
2023-12-07 23:57:11 +0900

    컨트롤러에서 알러트 메세지 추가

--- goodspro/src/main/java/com/goodspro/goodspro/mainController/MainController.java
+++ goodspro/src/main/java/com/goodspro/goodspro/mainController/MainController.java
@@ -2,7 +2,9 @@ package com.goodspro.goodspro.mainController;
 
 import java.util.List;
 
+import com.goodspro.goodspro.dto.MessageDto;
 import com.goodspro.goodspro.dto.SearchDto;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.goodspro.goodspro.dto.BoardDto;
 import com.goodspro.goodspro.service.BoardServiceImpl;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -28,11 +31,16 @@ public class MainController {
 	}
 	
 	@PostMapping("/write")
-	public String write(BoardDto boardDto) {
-		System.out.println(boardDto.getBoardTitle()+boardDto.getBoardContent());
+	public String write(BoardDto boardDto, Model model) {
 		boardService.insert(boardDto);
-		
-		return "redirect:/#signup";
+		MessageDto message = new MessageDto("게시글 생성이 완료되었습니다.", "/#signup", RequestMethod.GET, null);
+		return showMessageAndRedirect(message, model);
+	}
+
+	// 사용자에게 메시지를 전달하고, 페이지를 리다이렉트 한다.
+	private String showMessageAndRedirect(final MessageDto params, Model model) {
+		model.addAttribute("params", params);
+		return "common/messageRedirect";
 	}
 	
 }
